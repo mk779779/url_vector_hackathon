7b0813334c55d027741b6e13244c29db7b7f8373
masa73044 <108378047+masa73044@users.noreply.github.com>
2024-06-29 16:58:35 -0400

    initial

---
+++ vector1/app.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+
+from sentence_transformers import SentenceTransformer
+
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+embeddings = model.encode(sentences)
+print(embeddings)
